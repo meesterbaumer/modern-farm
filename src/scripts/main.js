eef113b4321806ffc9d778370f1564af718b8056
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-16 10:32:38 -0500

    Added all seeds

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,12 @@
 import { createPlan } from './plan.js'
+import { createSoybean } from "./seeds/soybean.js"
+import { createCorn } from "./seeds/corn.js"
+
+const soybeanSeed = createSoybean()
+const cornSeed = createCorn()
+
+console.log(soybeanSeed)
+console.log(cornSeed)
 
 console.log("Welcome to the main module")
 
