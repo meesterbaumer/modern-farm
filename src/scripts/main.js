6b8229160026c4358e5f6929c91ce7a5bf684015
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-17 15:51:48 -0500

    Completed printing to the Dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,15 @@ import { createPlan } from './plan.js'
 import { usePlants } from "./field.js"
 import { plantSeeds } from "./tractor.js"
 import { harvestPlants } from "./harvester.js"
+import { catalog } from "./catalog.js";
 
 const yearlyPlan = createPlan()
 const plantingTheSeeds = plantSeeds(yearlyPlan)
 const allThePlantsInTheField = usePlants()
 const plantsToHarvest = harvestPlants(allThePlantsInTheField)
+const theCatalog = catalog(plantsToHarvest)
+
+theCatalog
 
 console.log("Welcome to the main module")
 
